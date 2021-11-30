2ab620b5b8e79fe0d05ffaa8ac27c11a1f9b7250
antonio <antonior92@gmail.com>
2021-11-30 14:16:04 +0100

    update render from template script

--- scripts/render_from_template.py
+++ scripts/render_from_template.py
@@ -3,32 +3,39 @@ import yaml
 import os
 
 # Created datastructure similar to the one used by liquid in my website
-mydata = {}
-datafolder = '_data'
-for subdir, dirs, files in os.walk(datafolder):
-    for file in files:
-        filepath = subdir + os.sep + file
-        relative_path = os.path.relpath(filepath, datafolder)
-        print(subdir, dirs, files )
-        name, ext = os.path.splitext(relative_path)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Aplly liquid to a file')
+    parser.add_argument('file', help='input tex file.')
+    parser.add_argument('datafolder', help='folder containing data')
+    parser.add_argument('-o', '--output', default='out.txt', type=str,
+                        help='where to save the output')
+    args = parser.parse_args()
+    mydata = {}
+    for subdir, dirs, files in os.walk(args.datafolder):
+        for file in files:
+            filepath = subdir + os.sep + file
+            relative_path = os.path.relpath(filepath, args.datafolder)
+            name, ext = os.path.splitext(relative_path)
 
-        if ext == ".yml":
-            tags = os.path.normpath(name).split('/')
-            tags.reverse()
+            if ext == ".yml":
+                tags = os.path.normpath(name).split('/')
+                tags.reverse()
 
-            # Read YAML file
-            with open(filepath, 'r') as stream:
-                localdict = yaml.safe_load(stream)
-            # Assign it to dictionary
-            for t in tags[:-1]:
-                localdict = {t: localdict}
-            mydata[tags[-1]] = localdict
+                # Read YAML file
+                with open(filepath, 'r') as stream:
+                    localdict = yaml.safe_load(stream)
+                # Assign it to dictionary
+                for t in tags[:-1]:
+                    localdict = {t: localdict}
+                mydata[tags[-1]] = localdict
 
-template = Template(
-    "{% for paper in publications.mypapers %}"
-    "Hello, {{ paper.id }}!\n"
-    "{% endfor %}"
-)
-# TODO: Test with latex!
+    with open(args.file, "r") as f:
+        latex_template = f.read()
 
-print(template.render(**mydata))
+    template = Template(latex_template)
+
+    tex_output = template.render(**mydata)
+
+    with open(args.output, "w") as f:
+        f.write(tex_output)
