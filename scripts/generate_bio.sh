08af9db17907fb8b4c02628ed49a3c7cd5c73b53
antonio <antonior92@gmail.com>
2025-07-04 11:51:58 +0200

    Update publications
    So yml is generated together with bio

--- scripts/generate_bio.sh
+++
@@ -1,5 +0,0 @@
-
-PH="scripts/"
-pybtex-convert _publicationlist/refs.bib _data/publications/refs.yaml
-python $PH/clean_latex_yaml.py _data/publications/refs.yaml  _data/publications/refs.yml
-python $PH/replace_journal_names.py  _data/publications/refs.yml  _data/publications/refs.yml
